aa9414d20f432d1e9e4b37e5286c1438833d5788
SajeedAninda <sajaninda@gmail.com>
2023-09-02 20:15:27 +0600

    Stylized The Modal

--- scripts/index.js
+++ scripts/index.js
@@ -61,8 +61,75 @@ let cardDetails = async (cardID) => {
     url = `https://openapi.programming-hero.com/api/ai/tool/${cardID}`;
     let res = await fetch(url);
     let data = await res.json();
-    console.log(data);
+    let cardDetailedData=data.data;
+    // console.log(cardDetailedData);
 
+    // MODAL CONTAINER 
+    let modalContainer=document.querySelector("#modal-container");
+    // console.log(modalContainer);
+    modalContainer.innerHTML=   `
+    <div class="flex justify-center items-center">
+    <div class="left bg-[#eb57570d] rounded-xl py-6 px-4 flex flex-col justify-center items-center flex-1 space-y-8 w-[450px] h-[400px]">
+        <div class="top">
+            <h3 class="text-lg font-bold text-[#111]">${cardDetailedData?.description}</h3>
+        </div>
+
+
+        <div class="middle flex flex-row gap-3">
+            <div
+                class="div text-sm w-[100px] h-[80px] text-[#03A30A] bg-white p-6  rounded-xl font-bold text-center flex items-center justify-center">
+                <p>${cardDetailedData?.pricing[0]?.price} <br>
+                <span>${cardDetailedData?.pricing[0]?.plan} </span>
+                </p>
+
+            </div>
+            <div
+                class="div text-sm w-[100px] h-[80px] text-[#F28927] bg-white p-6 rounded-xl font-bold text-center flex items-center justify-center">
+                <p>${cardDetailedData?.pricing[1]?.price} <br>
+                <span>${cardDetailedData?.pricing[2]?.plan} </span>
+                </p>
+            </div>
+            <div
+                class="div text-sm w-[100px] h-[80px] text-[#EB5757] bg-white p-6 rounded-xl font-bold text-center flex items-center justify-center">
+                <p class="text-xs">${cardDetailedData?.pricing[2]?.price}<br>
+                <span>${cardDetailedData?.pricing[2]?.plan} </span>
+                </p>
+            </div>
+        </div>
+
+        <div class="bottom flex gap-8">
+            <div>
+                <h2 class="text-xl text-[#111] font-bold">Features</h2>
+                <ul class="text-[#585858] list-disc ml-4">
+                    <li>${cardDetailedData?.features[1].feature_name}</li>
+                    <li>${cardDetailedData?.features[2].feature_name}</li>
+                    <li>${cardDetailedData?.features[3].feature_name}</li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="text-xl text-[#111] font-bold">Integrations</h2>
+                <ul class="text-[#585858] list-disc ml-4">
+                    <li>${cardDetailedData?.integrations[0]}</li>
+                    <li>${cardDetailedData?.integrations[1]}</li>
+                    <li>${cardDetailedData?.integrations[2]}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="right flex-1 border rounded-xl border-gray-400 py-6 px-4 w-[450px] h-[400px] flex flec-col items-center justify-center text-center">
+    <div>
+        <img class="w-[9/12] h-[9/12]" src="${cardDetailedData.image_link[0]}" alt="">
+        <h2 class="text-xl font-bold text-[#111]">
+        ${cardDetailedData.input_output_examples[0].input}
+        </h2>
+        <p class="text-bg" >${cardDetailedData?.input_output_examples[0].output}</p>
+    </div>
+</div>
+    `
 
     ai_modal.showModal()
 }
